4bd56c27cf24888912c2ea9ba337564d34a1a8c7
ikysepertiwibu <94022360+ikysepertiwibu@users.noreply.github.com>
2022-08-19 00:39:34 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,16 +21,16 @@ global.auto_welcome_msg = true
 global.auto_leave_msg = true
 global.prefix = "."
 global.language = "ind"
-global.ownerNumberr = "62887435047326"
-global.ownerbot = ['62887435047326@s.whatsapp.net','918156874290@s.whatsapp.net'] // .owner
+global.ownerNumberr = "6289618728196"
+global.ownerbot = ['6289618728196@s.whatsapp.net','6287892870587@s.whatsapp.net'] // .owner
 global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
-global.botname = "Alphabot"
+global.botname = "NoerBhot"
 global.thumb = "thumbnail_1.jpg"
 global.thumbnail ="thumbnail_2_1.jpg"
 global.fakethumb ="thumbnail_1.jpg"
 global.donasi ="donasi.jpg"
 global.captionpay ="Scan untuk melakukan pembayaran"
-global.fake = "「  ALPHABOT  」\nCreated By ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×"
+global.fake = "「  NOERBOT  」\nCreated By ᴹᴿ᭄ NoerrKyFocོ ×፝֟͜×"
 global.alphaVersion = [2, 2143, 3] //WhatsApp web version
 
 /* Available type menu
